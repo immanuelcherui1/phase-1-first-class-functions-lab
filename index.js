fc3697b95f2b3589925da2621f6ee25f0534a68c
immanuelcherui1 <mmanuelcherui1@gmail.com>
2023-12-15 04:06:39 +0300

    Completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+// returnFirstTwoDrivers
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+
+const returnFirstTwoDrivers = () => {
+    return drivers.slice(0, 2)
+}
+
+
+// returnLastTwoDrivers
+const returnLastTwoDrivers = () => {
+    return drivers.slice(2, 4)
+}
+
+
+// selectingDrivers
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+
+// createFareMultiplier
+const createFareMultiplier = (integerArgument) => {
+    return (fare) => {
+        return fare * integerArgument;
+    }
+}
+
+
+
+// fareDoubler 
+const fareDoubler = createFareMultiplier(2);
+
+// fareTripler
+const fareTripler = createFareMultiplier(3);
+
+
+// selectDifferentDrivers
+const selectDifferentDrivers = (drivers, eitherDriversArray) => {
+    return eitherDriversArray(drivers);
+}
